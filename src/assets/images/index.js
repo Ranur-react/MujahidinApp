59807de120803d8864ab837d3cdb451f1484c573
Ranur-react <rahmatnur845@gmail.com>
2021-07-26 21:15:09 +0700

    backend saja

--- src/assets/images/index.js
+++
@@ -1,4 +0,0 @@
-import Logo from './logo.png'
-import UserImage from './userImage.png'
-
-export {Logo, UserImage}
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import BottomNavigator from './BottomNavigator'
-import ItemHome from './ItemHome'
-import ItemInput from './ItemInput'
-
-export {BottomNavigator, ItemHome, ItemInput}
