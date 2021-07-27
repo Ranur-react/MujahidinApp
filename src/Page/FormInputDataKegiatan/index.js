c1995caf0f254878e0f6ad1ced3f29022c5effae
Ranur-react <rahmatnur845@gmail.com>
2021-07-27 09:17:47 +0700

    Date Picker

--- src/Page/FormInputDataKegiatan/index.js
+++ src/Page/FormInputDataKegiatan/index.js
@@ -1,44 +1,65 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, TextInput } from 'react-native';
+import { View, Text, Platform,StyleSheet, TouchableOpacity,Modal, ScrollView, TextInput,Dimensions } from 'react-native';
 import { WARNA_UTAMA, WARNA_DISABLE, WARNA_SEKUNDER, WARNA_TEKS, TEKS_SIZE, TEKS_SIZE_TITTLE } from '../../utils/constant'
-import { API_kegiatanmesjid,API_PEMATERI } from '../../utils/api';
+import { API_kegiatanmesjid, API_PEMATERI, API_KEGIATAN } from '../../utils/api';
 import { IconBack, IconData } from '../../assets';
 import { showToastWithGravityAndOffset } from '../../components/_Toasview';
-import Combobox from '../../components/_comobox';
-// import {
-//   Select,
-//   CheckIcon,
-// } from "native-base";
+import SelectInputNative from '../../components/_comboBox';
+// import SelectInputNativeKegiatan from '../../components/_comboBoxKegiatan';
+import { Picker, DatePicker } from 'react-native-wheel-pick';
+const isIos = Platform.OS === 'ios';
 class FormInputDataKegiatan extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       secureTextEntry: true,
+      modalVisible: false,
       iconName: 'eye',
       kode_kegiatan: '',
       nama_kegiatan: '',
+      kode_datakegiatan: '',
       hari_kegiatan: '',
       waktu_kegiatan: '',
       idpematerikegiatan: '',
     }
   }
-  LoadData=()=>{
-    const GET= async()=>{
-      var a=API_PEMATERI();
-      if(a.status){
+  LoadData = () => {
+    const GETPEMATERI = async () => {
+      console.log("caf");
+      var a = await API_PEMATERI();
+      if (a.status) {
         showToastWithGravityAndOffset("Request pemateri succes");
-        this.setState({pemateri:a.data})
-      }else{
+        this.setState({ pemateri: a.data })
+      } else {
         showToastWithGravityAndOffset(a);
       }
     }
-    GET()
+    const GETPKEGIATAN = async () => {
+      console.log("cafkegiatan");
+      var a = await API_KEGIATAN();
+      if (a.status) {
+        showToastWithGravityAndOffset("Request pemateri succes");
+        this.setState({ kegiatan: a.data })
+      } else {
+        showToastWithGravityAndOffset(a);
+      }
+    }
+    GETPKEGIATAN();
+    GETPEMATERI();
   }
-UNSAFE_componentWillMount(){
-this.LoadData();
-}
+  UNSAFE_componentWillMount() {
+    this.LoadData();
+  }
   render() {
+    const pilihPemateri = (e) => {
+      console.log(e);
+      this.setState({ idpematerikegiatan: e })
+    }
+    const pilihKegiatan = (e) => {
+      console.log(e);
+      this.setState({ kode_datakegiatan: e })
+    }
     const simpan = () => {
       var POST = async () => {
         var resp = await API_kegiatanmesjid(this.state);
@@ -65,21 +86,63 @@ this.LoadData();
 
         <View style={styles.konten}>
           <ScrollView style={{ marginTop: 30, marginHorizontal: 20 }}>
-            <View style={{ marginBottom: 15 }}>
-              <Text style={styles.txtlabel}>Kode Kegiatan</Text>
-              <TextInput onChangeText={(w) => this.setState({ kode_kegiatan: w })} placeholder='Masukkan kode kegiatan' placeholderTextColor="grey" style={styles.txtinput} />
-            </View>
+            {
+              //   <View style={{ marginBottom: 15 }}>
+              //   <Text style={styles.txtlabel}>Kode Kegiatan</Text>
+              //   <TextInput onChangeText={(w) => this.setState({ kode_kegiatan: w })} placeholder='Masukkan kode kegiatan' placeholderTextColor="grey" style={styles.txtinput} />
+              // </View>
+            }
 
             <View style={{ marginBottom: 15 }}>
-              <Text style={styles.txtlabel}>Nama Kegiatan</Text>
-              <TextInput onChangeText={(w) => this.setState({ nama_kegiatan: w })} placeholder='Masukkan nama kegiatan' placeholderTextColor="grey" style={styles.txtinput} />
+              <Text style={styles.txtlabel}>Pilih Kegiatan</Text>
+            </View>
+            <View>
+              {
+                <SelectInputNative lable="Pilih Jenis Kegiatan" lebar={'100%'} selectedValue={this.state.kode_datakegiatan} onSelectData={(e) => pilihKegiatan(e)} data={this.state.kegiatan} />
+
+              }
             </View>
 
             <View style={{ marginBottom: 15 }}>
               <Text style={styles.txtlabel}>Hari</Text>
-              <TextInput onChangeText={(w) => this.setState({ hari_kegiatan: w })} placeholder='Masukkan hari' placeholderTextColor="grey" style={styles.txtinput} />
+              <TextInput onFocus={(e) => this.setState({ modalVisible: true })} onChangeText={(w) => this.setState({ hari_kegiatan: w })} value={this.state.hari_kegiatan} placeholder='Masukkan hari' placeholderTextColor="grey" style={styles.txtinput} />
             </View>
+            <View>
+              {
+                <View>
+                  <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                      alert("Modal has been closed.");
+                      (e) => this.setState({ ViewDate: false })
+                    }}
+                  >
+                    <View style={styles.centeredView} >
+                      <TouchableOpacity onPress={(e) => this.setState({ modalVisible: false })} style={[styles.modalView, { opacity: 0.5 }]} ></TouchableOpacity>
+                      <View style={styles.modalView} onPress={() => console.log("xx")}>
 
+                        <DatePicker onTouchCancel={() => console.log("focus")}
+                          style={{ backgroundColor: 'white', height: '100%', width: isIos ? 300 : undefined }}
+                          minimumDate={new Date('2015-01-01')}
+                          maximumDate={new Date('2030-12-12')}
+                          onDateChange={date => { this.setState({ hari_kegiatan: new Date(date).toISOString().slice(0, 10) }) }}
+                        />
+                        <TouchableOpacity
+                          style={[styles.button]}
+                          onPress={(e) => this.setState({ modalVisible: false })}
+                        >
+                          <Text style={styles.textStyle}>OK</Text>
+                        </TouchableOpacity>
+                      </View>
+                      <TouchableOpacity onPress={(e) => this.setState({ modalVisible: false })} style={[styles.modalView, { opacity: 0 }]} ></TouchableOpacity>
+                    </View>
+                  </Modal>
+
+                </View>
+              }
+            </View>
             <View style={{ marginBottom: 15 }}>
               <Text style={styles.txtlabel}>Waktu</Text>
               <TextInput onChangeText={(w) => this.setState({ waktu_kegiatan: w })} placeholder='Masukkan waktu' placeholderTextColor="grey" style={styles.txtinput} />
@@ -87,10 +150,12 @@ this.LoadData();
 
             <View style={{ marginBottom: 15 }}>
               <Text style={styles.txtlabel}>Nama Pemateri</Text>
-              <TextInput onChangeText={(w) => this.setState({ namapem_kegiatan: w })} placeholder='Masukkan nama pemateri' placeholderTextColor="grey" style={styles.txtinput} />
+
             </View>
             <View style={{ marginBottom: 15 }}>
-              <Combobox pemateri={this.state.pemateri} />
+              {
+                <SelectInputNative lable="Pilih Pemateri" lebar={'100%'} selectedValue={this.state.idpematerikegiatan} onSelectData={(e) => pilihPemateri(e)} data={this.state.pemateri} />
+              }
             </View>
 
             <TouchableOpacity onPress={() => simpan()} activeOpacity={0.8} style={{ alignItems: 'center', marginTop: 20 }}>
@@ -104,13 +169,47 @@ this.LoadData();
   }
 }
 export default FormInputDataKegiatan
-
+const { height: HEIGHT } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: WARNA_UTAMA
+  },buttonClose: {
+    backgroundColor: "#2196F3",
+    height: TEKS_SIZE * 2,
+    width: TEKS_SIZE * 5
+  },textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 16,
   },
-
+  modalView: {
+    flex: 1,
+    width: '100%',
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    }, shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  centeredView: {
+    position: 'absolute',
+    display: 'flex',
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    width: '100%',
+    height: HEIGHT,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
   header: {
     flex: 1,
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,10 @@
 import { BASE_URL } from './constant';
 import { showToastWithGravityAndOffset } from '../components/_Toasview';
-
-export const API_user = async (res) => {
-  var e = res;
-  let dataReturn;
-  let formdata = new FormData();
-
-  formdata.append("kode_user", e.kode_user)
-  formdata.append("nama_user", e.nama_user)
-  formdata.append("username_user", e.username_user)
-  formdata.append("password_user", e.password_user)
-  formdata.append("level_user", e.level_user)
-  showToastWithGravityAndOffset("POST : " + BASE_URL)
-  let respond = await fetch(BASE_URL + "backendmujahiddinapp/datauser/insert.php", {
-    method: "POST",
-    headers: { 'Content-Type': "multipart/form-data" },
-    body: formdata,
+//GET
+export const API_KEGIATAN = async () => {
+  showToastWithGravityAndOffset("GET : " + BASE_URL)
+  let respond = await fetch(BASE_URL + "backendmujahiddinapp/dataKegiatan/", {
+    method: "GET",
   }).then(response => response.json()).then(responseJson => {
     console.log(responseJson);
     return responseJson
@@ -43,16 +32,44 @@ export const API_PEMATERI = async () => {
   return dataReturn;
 }
 
-const API_kegiatanmesjid = async (res) => {
+//POST
+export const API_user = async (res) => {
   var e = res;
   let dataReturn;
   let formdata = new FormData();
 
-  formdata.append("kode_kegiatan", e.kode_kegiatan)
-  formdata.append("nama_kegiatan", e.nama_kegiatan)
+  formdata.append("kode_user", e.kode_user)
+  formdata.append("nama_user", e.nama_user)
+  formdata.append("username_user", e.username_user)
+  formdata.append("password_user", e.password_user)
+  formdata.append("level_user", e.level_user)
+  showToastWithGravityAndOffset("POST : " + BASE_URL)
+  let respond = await fetch(BASE_URL + "backendmujahiddinapp/datauser/insert.php", {
+    method: "POST",
+    headers: { 'Content-Type': "multipart/form-data" },
+    body: formdata,
+  }).then(response => response.json()).then(responseJson => {
+    console.log(responseJson);
+    return responseJson
+  }).catch(error => {
+    console.log(error);
+    return error
+  });
+
+  dataReturn = respond;
+  return dataReturn;
+}
+
+
+export const API_kegiatanmesjid = async (res) => {
+  var e = res;
+  let dataReturn;
+  let formdata = new FormData();
+
+  formdata.append("kode_datakegiatan", e.kode_datakegiatan)
   formdata.append("hari_kegiatan", e.hari_kegiatan)
   formdata.append("waktu_kegiatan", e.waktu_kegiatan)
-  formdata.append("namapem_kegiatan", e.namapem_kegiatan)
+  formdata.append("idpematerikegiatan", e.idpematerikegiatan)
 
   let respond = await fetch(BASE_URL+"backendmujahiddinapp/datakegiatanmesjid/insert.php", {
     method: "POST",
